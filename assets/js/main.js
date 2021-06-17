cd21b4607c59a487b9bcdb9a957233473b5b0399
William Condori Quispe <williamccondori@outlook.com>
2021-06-17 11:44:24 -0500

    add media queries footer and dark theme support

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,60 @@ function scrollTop() {
 }
 
 window.addEventListener("scroll", scrollTop);
+
+/*----------  DARK THEME  ----------*/
+
+const themeButton = document.getElementById("theme-button");
+
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.body.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  document.body.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+/*----------  activate / deactivate with the button  ----------*/
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  // saving in the local storage
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*----------  SCROLL REVEAL ANIMATION  ----------*/
+
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 1000,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `.home__data, .home__img, 
+  .about__data, .about__img, 
+  .services__content, 
+  .menu__content, 
+  .app__data, .app__img,
+  .contact__data, .contact__button,
+  .footer__content`,
+  {
+    interval: 200,
+  }
+);
